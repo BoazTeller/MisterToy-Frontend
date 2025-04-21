47411d232daeba5d2e541c6e313e1036e04b450e
Boaz Teller <boaz.teller@gmail.com>
2025-04-21 03:03:02 +0300

    feat(app): add routes for ToyDetails and ToyEdit pages

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -7,6 +7,8 @@ import { store } from './store/store'
 
 import { AppHeader } from './cmps/AppHeader'
 import { ToyIndex } from './pages/ToyIndex'
+import { ToyDetails } from './pages/ToyDetails'
+import { ToyEdit } from './pages/ToyEdit'
 
 export function App() {
     return (
@@ -14,9 +16,14 @@ export function App() {
             <Router>
                 <section className="app main-layout">
                     <AppHeader />
+                    
                     <main>
                         <Routes>
                             <Route path="/" element={<ToyIndex />} />
+                            <Route path="/toy" element={<ToyIndex />} />
+                            <Route path="/toy/:toyId" element={<ToyDetails />} />
+                            <Route path="/toy/edit" element={<ToyEdit />} />
+                            <Route path="/toy/edit/:toyId" element={<ToyEdit />} />
                         </Routes>
                     </main>
                 </section>
